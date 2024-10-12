2ec28e7f18bf7112e516af2845778be9f1d80a0a
Jay Ryan <jaryan@redhat.com>
2024-10-12 03:41:15 +0000

    extra emphasis!

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello to the future of DevOps!!"
+    return "Hello to the future of DevOps!!!"
 
 if __name__ == '__main__':
     port = os.environ.get('FLASK_PORT') or 8080
